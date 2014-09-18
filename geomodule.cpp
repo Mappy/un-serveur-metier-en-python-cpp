b7d2e8451bfd175a9956b212b42e1a3360f8535e
Alexandre Bonnasseau <alexandre.bonnasseau@mappy.com>
2014-09-18 17:52:47 +0200

    Exposed GeoPoint class to Python

--- geomodule.cpp
+++ geomodule.cpp
@@ -2,8 +2,12 @@
 #include <boost/python/module.hpp>
 #include "geo.hpp"
 
+using namespace boost::python;
+
 BOOST_PYTHON_MODULE(geo)
 {
-    using namespace boost::python;
     def("geocoord2string", geocoord2string);
+    class_<GeoPoint>("GeoPoint", init<double, double>())
+        .def("distance", &GeoPoint::distance)
+    ;
 }
